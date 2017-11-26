a8eb0a353c6158a36daed996071909a63e16c1a4
Greg Orton <ortongreg@gmail.com>
2017-11-25 23:39:15 -0600

    comment for judge return values

--- src/main/groovy/com/gorton/Judge.groovy
+++ src/main/groovy/com/gorton/Judge.groovy
@@ -1,6 +1,12 @@
 package com.gorton
 
 class Judge{
+    /*
+    returns -1 for tie
+            0 for no winner yet
+            1 for RED
+            2 for BLACK
+     */
     int winner(Board board){
         boolean full = true
         board.columns.each {
